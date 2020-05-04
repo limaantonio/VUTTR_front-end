1d03ec0e7d80283087a3ee40fb62d779552710f8
limaantonio <limaantoniocarlos20@gmail.com>
2020-05-03 22:12:21 -0300

    Integração com api construida com node.js.

--- src/modals/add/index.js
+++ src/modals/add/index.js
@@ -1,18 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import './styles.css';
+import api from '../../services/api';
 
 export default function Add(){
+  const [title, setTitle] = useState('');
+  const [link , setLink] = useState('');
+  const [description, setDescription] = useState('');
+  const [tags, setTags] = useState('');
+  const history = useHistory();
+
+  async function handleAddTool(e){
+    e.preventDefault();
+
+    const data = {
+      title,
+      link,
+      description,
+      tags,
+    };
+
+    try{
+      await api.post('tools',data);
+      alert('Ferramenta salva com sucesso.');
+      history.goBack();
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+
   return(
     <div className="cont">
       <h1>+ ADD new tool</h1>
-      <div className="content"> 
-        <input placeholder="Tool name"/>
-        <input placeholder="Tool link"/>
-        <textarea placeholder="Tool description"/>
-        <input placeholder="Tags"/>
-        <button id="btnAdd">Add tool</button>
-      </div>
+      <form onSubmit={handleAddTool}className="content"> 
+        <input 
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          placeholder="Tool name"
+        />
+        <input 
+          value={link}
+          onChange={e => setLink(e.target.value)}
+          placeholder="Tool link"
+        />
+        <textarea 
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+          placeholder="Tool description"
+        />
+        <input 
+          value={tags}
+          onChange={e => setTags(e.target.value)} 
+          placeholder="Tags"
+        />
+        <button type="submit" id="btnAdd">Add tool</button>
+      </form>
     </div>
   )
 }
--- src/pages/list/index.js
+++ src/pages/list/index.js
@@ -1,49 +1,69 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {FiTrash2} from 'react-icons/fi';
-
-import './styles.css';
 import { Link } from 'react-router-dom';
 
+import './styles.css';
+import api from '../../services/api';
+
 export default function List(){
+  const [tools, setTools] = useState([]);
+ 
   
+  
+  useEffect(() => {
+   api.get('tools').then(response => setTools(
+       response.data
+     )
+   )
+  },[])
+  async function handleDeleteTools(id){
+    try{
+      await api.delete(`tools/${id}`);
+
+      const toolsIndex = tools.findIndex(t => t.id === id);
+      const tool = [...tools];
+
+      tool.splice(toolsIndex, 1);
+      setTools(tool);
+     
+     
+    }catch(err){
+      alert('Ocorreu um erro ao deletar essa ferramenta, tente novamente.')
+    }
+  }
+  
+
   return(
     <div className="container">
      <div className="section">
         <h1>VUTTR</h1>
         <h2>Very Userful Tools to Remember</h2>
+        <form>
+          <input id="inputSeach" placeholder="Search"></input>
+          <div id="caixa">
+            <input id="check"type="checkbox"/>
+            <label>search in tags only</label>
+          </div>
+          <Link to='/add'>
+            <button id="bntAdcionar">Adicionar</button>
+          </Link>
+        </form>
+        <ul>
+        {tools.map(tool => (
         
-        <input id="inputSeach" placeholder="Search"></input>
-        <Link to='/add'>
-         <button id="bntAdcionar">Adicionar</button>
-        </Link>
-      
-        <div className="card"> 
-          <h1>Notion</h1>
-          <button className="btnTrash">
+          <li key={tool.id} className="card">
+            <h1>{tool.title}</h1>
+            <p>{tool.description}</p>
+            {tool.tags.map(tag => (
+              <p id="tag">#{tag}</p>
+            ))}
+          <button onClick={() => handleDeleteTools(tool.id)} className="btnTrash">
             <FiTrash2 size={20}/>
           </button>
-          
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, corrupti? Veritati</p>
-          <p>orem ipsum dolor sit amet consectetu orem ipsum dolor sit amet consectetu</p>
-         
-        </div>
-        <div className="card"> 
-          <h1>Notion</h1>
-          <button className="btnTrash">
-          <FiTrash2 size={20}/>
-          </button>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, corrupti? Veritati</p>
-          <p>orem ipsum dolor sit amet consectetu orem ipsum dolor sit amet consectetu</p>
-        </div>
-        <div className="card"> 
-          <h1>Notion</h1>
-          <button className="btnTrash">
-          <FiTrash2 size={20}/>
-          </button>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, corrupti? Veritati</p>
-          <p>orem ipsum dolor sit amet consectetu orem ipsum dolor sit amet consectetu</p>
+          </li>
+        ))}   
+        </ul>
         </div>
      </div>
-    </div>
   )
 }
